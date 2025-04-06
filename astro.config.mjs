6a41e67926ff8a916c80759755b06ac11e95ef01
emadadel4 <emadadel1920@gmail.com>
2025-04-06 08:31:02 +0200

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://itt-co.github.io',
+    base: 'itt-co.github.io'
+});
